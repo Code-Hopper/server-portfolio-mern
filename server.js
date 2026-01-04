4adb67186ac8238adea449039f338a74e50fb801
“code-hopper” <ameykhondekar01@gmail.com>
2026-01-04 12:19:00 +0530

    Backdated commit message

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ const app = express()
 
 app.use(express.static("public"))
 
-let corsOption = {
-    origin: "*",
-    method: "*"
-}
+// let corsOption = {
+//     origin: "*",
+//     method: "*"
+// }
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json())
